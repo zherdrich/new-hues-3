917faef6f4e819e4ece3086405427b142a1c0b32
zherdrich <z@Zs-MacBook-Air.local>
2023-04-04 16:05:08 -0400

    apr4.1

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+const CLIENT_ID = "235cb2acedda452f85b6309dab557b25"
+const REDIRECT_URI = "http://localhost:3000"
+const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+const RESPONSE_TYPE = "token"
+
+
+
+
   return (
     <div className="App">
       <h1>new hues</h1>
-      <div>testing</div>
+
+
+      <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login</a>
     </div>
   );
 }
